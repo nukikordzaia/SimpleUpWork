5045c334376ea4a3c8cc031a46eaab46b9740b0d
nnukikordzaia <anuki.kordzaia@azry.com>
2022-04-22 00:29:43 +0400

    added Swagger-UI configuration.

--- core/src/main/java/com/simpleupwork/SimpleUpWorkApplication.java
+++ core/src/main/java/com/simpleupwork/SimpleUpWorkApplication.java
@@ -2,6 +2,10 @@ package com.simpleupwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,4 +16,11 @@ public class SimpleUpWorkApplication {
 		SpringApplication.run(SimpleUpWorkApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis(RequestHandlerSelectors.basePackage("com.simpleupwork"))
+			.build();
+	}
 }
