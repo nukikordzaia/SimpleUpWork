39c93ae582e6e2db2205cda69404c44eb185d1f3
nnukikordzaia <anuki.kordzaia@azry.com>
2022-04-25 14:07:42 +0400

    implemented RedirectSuccessHandler.

--- core/src/main/java/com/simpleupwork/security/RedirectSuccessHandler.java
+++ core/src/main/java/com/simpleupwork/security/RedirectSuccessHandler.java
@@ -1,4 +1,26 @@
 package com.simpleupwork.security;
 
-public class RedirectSuccessHandler {
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RedirectSuccessHandler implements AuthenticationSuccessHandler {
+
+	private final String successUrl;
+	private final RedirectStrategy strategy = new DefaultRedirectStrategy();
+
+	public RedirectSuccessHandler(String successUrl) {
+		this.successUrl = successUrl;
+	}
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+		strategy.sendRedirect(httpServletRequest, httpServletResponse, successUrl);
+	}
 }
